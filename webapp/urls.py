49a6e3ffe6e30dd00535967ca1933a71ad4a3449
Hinsteny <hinsteny@gmail.com>
2015-12-21 16:03:49 +0800

    #1 Initial Project

---
+++ webapp/urls.py
@@ -0,0 +1,16 @@
+# encoding=utf-8
+from django.conf.urls import url
+
+from . import views
+__author__ = 'Hinsteny'
+
+urlpatterns = [
+    # ex: /
+    url(r'^$', views.index, name='index'),
+    # ex: /5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
